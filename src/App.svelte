12bdd1f1bc1af7c73cba67b97ef9f26d4fa6f19b
Lawrence Zémour <lawrence.zemour@exagroup.biz>
2019-06-16 16:36:39 +0200

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	export let version: string;
+  let name = "World";
+  let test = process.versions.electron;
+</script>
+
+<style>
+	h1  {
+		color: purple;
+	}
+</style>
+
+<h1>Hello <span>{name}</span>!</h1>
+<button class="ui primary button">
+  Save
+</button>
+<button class="ui button">
+  Discard
+</button>
+<p>We are using Node.js version {process.versions.node},</p>
+<p>Chromium version {process.versions.chrome}</p>
+<p>and Electron version {process.versions.electron}.</p>
+<p>But most importantly <strong>Svelte</strong> version {version}!</p>
